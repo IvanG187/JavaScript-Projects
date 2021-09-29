5afce69c8f1291c478b084b0190f013a529ccfa1
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-09-29 13:57:53 -0400

    update

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,8 +1,15 @@
 window.alert("Dont Forget to click OK!");
+
 document.write("YOU HAVE TO CLICK THE BUTTON!!!!");
 
 function My_First_Function () { // Defining a function and naming it
     var str = "This text is green!"; //Defining a variable and giving it a sring value
     var result = str.fontcolor("green"); //using the fontcolor method on str variable
     document.getElementById("Green_Text", "p").innerHTML = result; // putting the value of the result into html element with Green_Text id
+}
+
+function myFunction(){
+    var sentence = "I am Learning";
+    sentence += " a lot from this!";
+    document.getElementById("Concatencate").innerHTML = sentence;
 }
