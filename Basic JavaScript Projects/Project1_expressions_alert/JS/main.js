12a11e54286ff644e696d4ac1824d9025fdf6409
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-09-29 13:36:34 -0400

    updated fix

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,6 @@
-var blues = "I have the blues.";
-var blues = blues.fontcolor("blues");
-document.write(blues);
+
+function My_First_Function () { // Defining a function and naming it
+    var str = "This text is green!"; //Defining a variable and giving it a sring value
+    var result = str.fontcolor("green"); //using the fontcolor method on str variable
+    document.getElementById("Green_Text").innerHTML = result; // putting the value of the result into html element with Green_Text id
+}
