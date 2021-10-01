72fce512ddaf5e62c430031d2d38979ea741a8f7
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-01 15:18:56 -0400

    updated

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,43 @@
+function countdown() {
+    var secounds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        TimeRanges.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's UP!");
+            clearTimeout(time);
+            TimeRanges.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
