282aaef5bb84d186e225e531987ecbe286582ac4
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-01 14:36:04 -0400

    updated

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,44 @@
+function Add_numbers_1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write (X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+if (6<7) {
+    document.write("The left number is smaller than the number on the right.")
+}
+function get_Date() {
+    if (new Date().getHours()< 18) {
+        document.getElementById("Greeting").innerHTML = "How are you Today?";
+    }
+}
+
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old Enough to Vote!";
+    }
+    document.getElementById("How_old_are_you").innerHTML = Vote;
+}
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is Morning";
+    }
+    else if (Time >= 12 == Time <18) {
+        Reply = "It is Afternoon";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_Day").innerHTML = Reply;
+}
