e0ce2c57b7c55a822301f6e4de4457ffb08a3943
IvanG187 <90492564+IvanG187@users.noreply.github.com>
2021-10-02 13:27:15 -0400

    updated

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -62,3 +62,17 @@ document.write(X);
     document.write("<br>" + X);
 }
 document.write("<br>" + X);
+
+
+  // Objects
+
+  let car = {
+      make: "Dodge ",
+      model: "Viper ",
+      year: "2021 ",
+      color: "Red ",
+      description : function() {
+          return "The car is a " + this.year + this.color + this.make + this.model;
+      }
+  };
+  document.getElementById("Car_Object").innerHTML = car.description();
